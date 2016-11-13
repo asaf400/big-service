aeaeeb69b25b2e3f7213806db5b07b150d9d4f5e
Asaf Levy <asaf400@gmail.com>
2016-11-13 16:01:10 +0200

    Finishing touches and comments

--- bigservice.py
+++ bigservice.py
@@ -2,11 +2,13 @@ from flask import Flask
 
 import counter
 
+# initialize flask application class with static folder override
 app = Flask(__name__)
 
-
+# route '/'  to invoke count() when method is GET
 @app.route('/', methods=["GET"])
 def count():
+    # return count attribute of a new counter.counter instance
     return str(counter.counter().count)
 
 
--- counter/__init__.py
+++ counter/__init__.py
@@ -1,9 +1,13 @@
 class counter:
+    # initialize class attribute
     count = 0
 
+    # on new instance
     def __init__(self):
+        # set class attribute count to itself +1
         counter.count += 1
         pass
 
     def get_count(self):
+        # return class attribute
         return counter.count
